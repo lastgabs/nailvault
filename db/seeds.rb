fafed1462a69a239e1cb9ba4b67f6c1cf8bc4978
Gabi Stefanini <lastgabs@users.noreply.github.com>
2017-03-18 22:57:36 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+supermoon_brand = Brand.create!(
+  name: "Supermoon Lacquer",
+  website: "https://www.thehowlingboutique.com/sml/",
+  maker: "Mardi Carruth",
+  country: "US",
+  status: :open,
+  instagram: "https://www.instagram.com/thehowlingboutique/",
+  facebook_page: "https://www.facebook.com/TheHowlingBoutique",
+)
+
+supermoon_collection = Collection.create!(
+  name: "Ready to Launch",
+  year: 2016,
+  brand: supermoon_brand,
+)
+
+supermoon_cosmic_dust = NailPolish.create!(
+  name: "Cosmic Dust",
+  collection: supermoon_collection,
+  brand: supermoon_brand
+)
+
+supermoon_galactic_halo = NailPolish.create!(
+  name: "Galactic Halo",
+  collection: supermoon_collection,
+  brand: supermoon_brand
+)
