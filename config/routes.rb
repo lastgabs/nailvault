6c40f6da79c7aa12aaeb5ad51f7522d34bd18ee8
Gabi Stefanini <lastgabs@users.noreply.github.com>
2017-03-19 22:29:02 -0400

    Add home controller and view to prevent sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
   devise_for :users
-  root 'nail_polishes#index'
 
   resources :nail_polishes
   resources :collections
   resources :brands
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
