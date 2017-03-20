05ed5dc1c10dac8f38e97ad6e57347d06a2315b6
Gabi Stefanini <lastgabs@users.noreply.github.com>
2017-03-19 21:44:07 -0400

    Add devise and configure users

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170319043530) do
+ActiveRecord::Schema.define(version: 20170319145816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "brands", id: :serial, force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.string "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "maker"
-    t.string "country", null: false
+    t.string "country"
     t.integer "status", default: 0, null: false
     t.string "instagram"
     t.string "facebook_page"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20170319043530) do
   end
 
   create_table "collections", id: :serial, force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.integer "year"
     t.integer "brand_id"
     t.datetime "created_at", null: false
@@ -70,10 +70,12 @@ ActiveRecord::Schema.define(version: 20170319043530) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "collections", "brands"
